09f075b4f2264f2ff128d49cfc87f59f3f23ee29
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-17 12:54:35 +0530

    .

--- splunk_fowd_install.sh
+++ splunk_fowd_install.sh
@@ -60,23 +60,21 @@ read -r -p "Do you want to configure destination server for monitoring?: (y/N): 
                if [[ -z $userinput ]]; then
                         if [[ $userinput == Y || $userinput == y ]]; then
                            forarderdestconfig
+
+                              read -r -p "Do you want add Monitor? (Y/n): " userinputmonitor
+                              if [[ -z $userinputmonitor ]]; then
+                                       if [[ $userinputmonitor == Y || $userinputmonitor == y ]]; then
+                                          forarderdmonconfig
+                                       elif [[ $userinputmonitor == N || $userinputmonitor == n ]]; then
+                                          echo "you can add monitor later using - splunk add monitor $addmonitor"
+                                       fi      
+                              else  forarderdmonconfig
+                              fi  
+
                         elif [[ $userinput == N || $userinput == n ]]; then
                            echo "you can congigure destination later using the command - splunk add forward-server $destination_IP:$destport "
                         fi      
                else  forarderdestconfig
-               fi  
-
-
-
-read -r -p "Do you want add Monitor? (Y/n): " userinputmonitor
-
-               if [[ -z $userinputmonitor ]]; then
-                        if [[ $userinputmonitor == Y || $userinputmonitor == y ]]; then
-                           forarderdmonconfig
-                        elif [[ $userinputmonitor == N || $userinputmonitor == n ]]; then
-                           echo "you can add monitor later using - splunk add monitor $addmonitor"
-                        fi      
-               else  forarderdmonconfig
                fi  
 
 
